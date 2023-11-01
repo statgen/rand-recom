03b1ec7939bcc0b4e289ce07b7460eba44413791
jonathonl <jonathonl@users.noreply.github.com>
2023-11-01 15:54:47 -0400

    Adds support for chrX.

--- main.cpp
+++ main.cpp
@@ -12,6 +12,17 @@
 #include <savvy/reader.hpp>
 #include <savvy/writer.hpp>
 
+template<typename Prng>
+void increment_rand(std::vector<std::size_t>::iterator& it, std::vector<std::size_t>& vec, Prng& r)
+{
+  ++it;
+  if (it == vec.end())
+  {
+    std::shuffle(vec.begin(), vec.end(), r);
+    it = vec.begin();
+  }
+}
+
 int main(int argc, char** argv)
 {
   if (argc < 2)
@@ -30,12 +41,9 @@ int main(int argc, char** argv)
   if (in.samples().empty())
    return std::cerr << "Error: no samples in input file\n", EXIT_FAILURE;
 
-  std::size_t n_haps = in.samples().size() * 2; // Assuming diploid
+  //std::size_t n_haps = in.samples().size() * 2; // Assuming diploid
 
-  std::vector<std::size_t> hap_mapping(n_haps);
-  std::iota(hap_mapping.begin(), hap_mapping.end(), 0);  
 
-  std::vector<std::size_t> hap_switch_cnts(n_haps);
 
   auto ids = in.samples();
   for (std::size_t i = 0; i < ids.size(); ++i)
@@ -47,17 +55,47 @@ int main(int argc, char** argv)
   std::cerr << "Recom prob: " << recom_prob << std::endl;
   //std::default_random_engine prng(seed);
   std::mt19937_64 prng(seed);
-  std::shuffle(hap_mapping.begin(), hap_mapping.end(), prng);
   std::geometric_distribution<std::int64_t> geom_dist(recom_prob);
-  std::vector<std::size_t> random_hap_idx = hap_mapping;
-  auto random_hap_idx_it = random_hap_idx.end();
-  std::int64_t geom_draw = geom_dist(prng);
 
   std::vector<std::int8_t> gt, gt_shuffled;
   savvy::variant rec;
-  while (in >> rec)
+  if (!(in >> rec))
+    return std::cerr << "Error: empty VCF\n", EXIT_FAILURE;
+  rec.get_format("GT", gt);
+
+  std::vector<std::size_t> random_hap_idx;
+  random_hap_idx.reserve(gt.size());
+  for (std::size_t i = 0; i < gt.size(); ++i)
   {
-    std::int64_t remaining_hap_positions_in_row = n_haps;
+    if (!savvy::typed_value::is_end_of_vector(gt[i]))
+      random_hap_idx.emplace_back(i);
+  }
+
+
+  std::vector<std::size_t> hap_mapping(gt.size());
+  std::iota(hap_mapping.begin(), hap_mapping.end(), 0);
+  std::vector<std::size_t> hap_switch_cnts(gt.size());
+
+  std::shuffle(random_hap_idx.begin(), random_hap_idx.end(), prng);
+  auto random_hap_idx_it = random_hap_idx.begin();
+
+  for (std::size_t i = 0; i < gt.size(); ++i)
+  {
+    if (!savvy::typed_value::is_end_of_vector(gt[i]))
+      std::swap(hap_mapping[i], hap_mapping[*(random_hap_idx_it++)]);
+  }
+
+  std::shuffle(random_hap_idx.begin(), random_hap_idx.end(), prng);
+  random_hap_idx_it = random_hap_idx.begin();
+
+
+
+
+  std::int64_t geom_draw = geom_dist(prng);
+
+  do //while (in >> rec)
+  {
+    std::int64_t remaining_hap_positions_in_row = random_hap_idx.size();
 
     while (true)
     {
@@ -68,14 +106,12 @@ int main(int argc, char** argv)
       }
       else
       {
-        if (random_hap_idx_it == random_hap_idx.end())
-        {
-          std::shuffle(random_hap_idx.begin(), random_hap_idx.end(), prng);
-          random_hap_idx_it = random_hap_idx.begin();
-        } 
- 
-        std::size_t hap1 = *(random_hap_idx_it++);
-        std::size_t hap2 = *(random_hap_idx_it++);
+        std::size_t hap1 = *random_hap_idx_it;
+        increment_rand(random_hap_idx_it, random_hap_idx, prng);
+
+        std::size_t hap2 = *random_hap_idx_it;
+        increment_rand(random_hap_idx_it, random_hap_idx, prng);
+
         // if (hap1 != hap2) This will never happen with shuffle approach
         {
           ++hap_switch_cnts[hap1];
@@ -90,20 +126,27 @@ int main(int argc, char** argv)
     }
 
     rec.get_format("GT", gt);
-    if (gt.size() != n_haps)
-      return std::cerr << "Error: GT must be diploid\n", EXIT_FAILURE;
+    if (gt.size() != hap_mapping.size())
+      return std::cerr << "Error: inconsistent ploidy\n", EXIT_FAILURE;
 
     gt_shuffled.resize(gt.size());
-    for (std::size_t i = 0; i < n_haps; ++i)
+    for (std::size_t i = 0; i < gt.size(); ++i)
       gt_shuffled[hap_mapping[i]] = gt[i];
     
     rec.set_format("GT", gt_shuffled);
    
     out << rec;
-  }
+  } while (in >> rec);
 
   if (in.bad() || !out.good())
     return std::cerr << "Error: I/O failure\n", EXIT_FAILURE;
+
+
+  for (std::size_t i = 0; i < gt.size(); ++i)
+  {
+    if (savvy::typed_value::is_end_of_vector(gt[i]) && hap_switch_cnts[i] != 0)
+      return std::cerr << "Error: switch occurred at EOV position in last variant (make sure all variants have consistent ploidy)\n", EXIT_FAILURE;
+  }
 
   for (std::size_t i = 0; i < in.samples().size(); ++i)
     std::cerr << in.samples()[i] << "\t" << hap_switch_cnts[i*2] << "\t" << hap_switch_cnts[i*2+1] << "\n";
